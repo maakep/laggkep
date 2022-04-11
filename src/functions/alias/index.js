299e03b449fc2428da6d2fe553bed4fa74295de0
Maakep <schon.fredrik@gmail.com>
2022-04-11 14:48:46 +0200

    add... too much

--- src/functions/alias/index.js
+++ src/functions/alias/index.js
@@ -1,13 +1,98 @@
 const { Firestore } = require("@google-cloud/firestore");
-
 const firestore = new Firestore();
+const collectionRef = firestore.collection("aliases");
 
-exports.alias = async (request, response) => {
-  const col = firestore.collection("aliases");
-  const res = await col.get();
+const ROUTES = {
+  DELETE: {
+    "/alias": deleteAliases,
+    "/alias/id": deleteId,
+  },
+  POST: {
+    "/alias": setAliases,
+  },
+  GET: {
+    "/alias": getAlias,
+    "/alias/query": getQuery,
+    "/alias/all": getAliases,
+  },
+};
 
-  const docs = res.docs;
-  const message = "Hello World!" + docs?.[0]?.data()[0];
-  console.log(res);
-  response.status(200).send(message);
+function extractData(snapshot) {
+  const data = [];
+  snapshot.forEach((x) => data.push(x.data()));
+  return data;
+}
+
+async function deleteId(req) {
+  const { id } = req.body;
+  const doc = collectionRef.doc(id);
+  const result = await doc.delete();
+  console.log(result);
+
+  return { status: 200, message: "Very great" };
+}
+
+async function getAlias(req) {
+  const { aliases } = req.body;
+  const snapshot = await collectionRef
+    .where("aliases", "array-contains-any", aliases)
+    .get();
+  const data = extractData(snapshot);
+  return data;
+}
+
+// https://firebase.google.com/docs/firestore/query-data/queries
+async function getQuery(req) {
+  const { args } = req.body;
+  const snapshot = await collectionRef.where("aliases", args[0], args[1]).get();
+  const data = extractData(snapshot);
+  return data;
+}
+
+async function getAliases(req) {
+  const snapshot = await collectionRef.get();
+  const data = extractData(snapshot);
+  return data;
+}
+
+async function setAliases(req) {
+  const { id, aliases } = req.body;
+
+  const docRef = collectionRef.doc(id);
+  const doc = await docRef.get();
+
+  if (doc.exists) {
+    const existingAliases = doc.data().aliases;
+    await docRef.update({
+      id: id,
+      aliases: [...new Set([...existingAliases, ...aliases])],
+    });
+  } else {
+    await docRef.set({
+      id: id,
+      aliases: aliases,
+    });
+  }
+
+  return { status: 200, message: "Very great" };
+}
+
+async function deleteAliases(req) {
+  const { id, aliases } = req.body;
+  const docRef = collectionRef.doc(id);
+  const doc = await docRef.get();
+
+  if (doc.exists) {
+    const existingAliases = doc.data().aliases;
+    await docRef.update({
+      id: id,
+      aliases: [...existingAliases].filter((x) => !aliases.includes(x)),
+    });
+  }
+}
+
+exports.alias = async (req, response) => {
+  console.log(req.method, req.path);
+  const result = await ROUTES[req.method][req.path](req);
+  response?.status(200).json(result);
 };
