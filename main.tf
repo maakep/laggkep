328edb7671d525d39fb69376adbc55d0923ccf0d
Maakep <schon.fredrik@gmail.com>
2022-04-14 17:52:22 +0200

    add action for terraforming triggers

--- main.tf
+++ main.tf
@@ -1,15 +1,39 @@
 provider "google" {
   project = "laggkep"
-  region = "europe-west1"
+  region  = "europe-west1"
 }
 
 terraform {
   backend "gcs" {
-    bucket = "terraform-state-bucket"
-    prefix = "laggkep"
+    bucket = "laggkep-terraform"
   }
 }
 
-resource "google_cloudbuild_trigger" "first" {
+resource "google_cloudbuild_trigger" "alias" {
+  name            = "deploy-alias"
+  filename        = "src/functions/alias/cloudbuild.json"
+  included_files  = ["src/functions/alias/**"]
+  service_account = var.service_account
+  github {
+    owner = "maakep"
+    name  = "laggkep"
+    push {
+      branch = "^main$"
+    }
+  }
+}
 
+
+resource "google_cloudbuild_trigger" "result" {
+  name            = "deploy-result"
+  filename        = "src/functions/result/cloudbuild.json"
+  included_files  = ["src/functions/result/**"]
+  service_account = var.service_account
+  github {
+    owner = "maakep"
+    name  = "laggkep"
+    push {
+      branch = "^main$"
+    }
+  }
 }
