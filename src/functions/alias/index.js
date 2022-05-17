8f1cce1ddb3e9dcb9667b60646ad9b6c12e494a4
Maakep <schon.fredrik@gmail.com>
2022-05-17 23:53:18 +0200

    tighten validation on allowed aliases

--- src/functions/alias/index.js
+++ src/functions/alias/index.js
@@ -73,11 +73,21 @@ async function getAllAliases(req) {
 async function setAliases(req) {
   const { id, aliases } = req.body;
 
+  const valid = validate(id, aliases);
+  if (!valid) {
+    return { status: 400, message: "Invalid characters: [a-z0-9åäö_-]" };
+  }
+
   const docRef = collectionRef.doc(id);
   const doc = await docRef.get();
 
   if (doc.exists) {
     const existingAliases = doc.data().aliases;
+
+    if (existingAliases.length > 50) {
+      return { status: 403, message: "Too many, delete some" };
+    }
+
     await docRef.update({
       id: id,
       aliases: [...new Set([...existingAliases, ...aliases])],
@@ -90,6 +100,15 @@ async function setAliases(req) {
   }
 
   return { status: 200, message: "Very great" };
+}
+
+function validate(id, aliases) {
+  return (
+    !isNaN(id) &&
+    aliases.every(
+      (x) => typeof x == "string" && x.match(/^[a-z0-9åäö_-]{2,35}$/g)
+    )
+  );
 }
 
 async function deleteAliases(req) {
